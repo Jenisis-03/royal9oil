316b0c0f39477e1ae24016a5d637100a1e29827a
Jenisis-03 <111039096+Jenisis-03@users.noreply.github.com>
2024-12-30 12:32:27 +0530

    New Laaayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,19 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "../app/Components/app-sidebar"; // Importing AppSidebar
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <SidebarProvider>
           {/* Render AppSidebar for the sidebar content */}
           <AppSidebar />
